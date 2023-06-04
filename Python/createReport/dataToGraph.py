ba8061987bbd021894bad81c54246616af0481ce
Jodi-321 <jodim321@gmail.com>
2023-06-03 22:36:25 -0500

    Update dataToGraph.py
    updating script to add more data reported on

--- Python/createReport/dataToGraph.py
+++ Python/createReport/dataToGraph.py
@@ -2,37 +2,46 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import jinja2
 import time
+import random
 
 '''
 This script pulls IP addresses from excel spreadsheet(FAKE DATA)
 Then sorts data based on IP Class
 Then graphs number of IPs for each class
+five sites are picked at random each time the report is ran
+the details ofr hte sites display in a table in the generated report
+
                                                 Jodi
 '''
 
+#pull time report is generated
 def getTime():
     current_time = time.ctime()
     return current_time
 
-def get_data():#excelData):
+#pulls data stored in Excel spreadsheet for report
+def get_data(dataStor):#excelData):
     pulledData = []
     path = 'data/MOCK_DATA.xlsx'
 
     #listing spreadsheet headers
     fields = ['site names','Networks','Addresses']
+
     #pulling data from spreadsheet
     data = pd.read_excel(open(path, 'rb'))
-    #storing 'Networks' column in variable
+
+    #storing excel columns in variable
     excelNetworksData = (data.Networks)
+    excelSiteNamesData = (data.SiteNames)
+    excelAddrData = (data.Addresses)
 
-
+    #appending excel data to data array
     pulledData.append(excelNetworksData)
+    pulledData.append(excelSiteNamesData)
+    pulledData.append(excelAddrData)
 
     # returns data that will be graphed
     return pulledData
-
-
-
 
 def sortNGraph(sortData):
     classAarray = [1,2,3,4,5,7,8]
@@ -43,7 +52,6 @@ def sortNGraph(sortData):
     clasB = []
     clasC = []
     clasU = []
-
 
     for x in sortData:
         #Each line is checked to IP Class, and stored in appropriate array
@@ -73,16 +81,12 @@ def createGraph(graphData):
 
     ax.set_ylabel("Number of IPs")
     ax.set_title('Site IPs')
-    #ax.legend(title='Legend')
 
-    #plt.savefig('foo.pdf')
     #plt.show()
     imgPath = 'data/graph.png'
     plt.savefig(imgPath)
 
-def createReport(dataStore,genTime):
-    #print(dataStore)
-
+def createReport(dataStore,genTime,pickedSites):
     classA = dataStore[0]
     classB = dataStore[1]
     classC = dataStore[2]
@@ -91,7 +95,23 @@ def createReport(dataStore,genTime):
     graphPng = 'graph.png'
     companyLogo = 'companyLogo.jpg'
 
-    htmlVars = {'CompanyLogo':companyLogo,'graphImg':graphPng,'DateTime':DateTime,'classA':classA,'classB':classB,'classC':classC,'classU':classU}
+    #print(pickedSites[1])
+    siteNumOne = pickedSites[1]
+    siteNumTwo = pickedSites[2]
+    siteNumThree = pickedSites[3]
+    siteNumFour = pickedSites[4]
+    siteNumFive = pickedSites[5]
+
+    #Storing values in html tags
+    htmlVars = {
+                'CompanyLogo':companyLogo,'graphImg':graphPng,'DateTime':DateTime,
+                'classA':classA,'classB':classB,'classC':classC,'classU':classU,
+                'siteNumOne':siteNumOne[3],'siteNameOne':siteNumOne[0],'siteNetworkOne':siteNumOne[1],'siteAddrOne':siteNumOne[2],
+                'siteNumTwo':siteNumTwo[3],'siteNameTwo':siteNumTwo[0],'siteNetworkTwo':siteNumTwo[1],'siteAddrTwo':siteNumTwo[2],
+                'siteNumThree':siteNumThree[3],'siteNameThree':siteNumThree[0],'siteNetworkThree':siteNumThree[1],'siteAddrThree':siteNumThree[2],
+                'siteNumFour':siteNumFour[3],'siteNameFour':siteNumFour[0],'siteNetworkFour':siteNumFour[1],'siteAddrFour':siteNumFour[2],
+                'siteNumFive':siteNumFive[3],'siteNameFive':siteNumFive[0],'siteNetworkFive':siteNumFive[1],'siteAddrFive':siteNumFive[2],
+                }
 
     template_loader = jinja2.FileSystemLoader('./')
     template_env = jinja2.Environment(loader=template_loader)
@@ -106,24 +126,54 @@ def createReport(dataStore,genTime):
     html_file.write(output_text)
     html_file.close()
 
+def sitePicker(dataStor,allExcelData):
+
+    count = 0
+    siteNames = list(allExcelData[1])
+    topFive = list(random.sample(dataStor,5))
+    finalSiteDict = {}
+
+    #print(topFive)
+    for x in siteNames:#topFive:
+
+        if x in topFive:
+            addPad = siteNames.index(x)
+            xSiteName = allExcelData[1][addPad]
+            xNetwork = allExcelData[0][addPad]
+            xAddress = allExcelData[2][addPad]
+            xSiteNum = addPad +2
+
+            topFiveDetails = (xSiteName,xNetwork,xAddress,xSiteNum)
+
+            count += 1
+            finalSiteDict.update({count:topFiveDetails})
+
+    return(finalSiteDict)
+
 def main():
     genTime = getTime()
-    dataArray = [get_data(),genTime]
+    dataArray = []
+    dataArray.append(get_data(dataArray))
 
-    # store IP with CIDRs in set
-    networkList = set(dataArray[0][0])
+    dataArray = [dataArray, genTime]
 
-    # Send to function to sort IPs
+    siteList = list(dataArray[0][0][1])
+    pickedSites = sitePicker(siteList,dataArray[0][0])
+
+    #store IP with CIDRs in set
+    networkList = set(dataArray[0][0][0])
+
+    #Send to function to sort IPs
     getGraphData = sortNGraph(networkList)
 
-
-    # send to function to create graph
+    #send to function to create graph
     createGraph(getGraphData)
 
+    dataArray.append(pickedSites)
+
     #create report with graphdata, and current time being sent to function
-    createReport(getGraphData,dataArray[1])
+    createReport(getGraphData,dataArray[1],dataArray[2])
 
 if __name__ == '__main__':
     main()
-
     print("Script Complete!")
